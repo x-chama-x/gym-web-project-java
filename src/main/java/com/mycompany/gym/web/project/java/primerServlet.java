b86a0e2c4e308a9aa9a80859d9f52f60c3dbce6b
x_chama_x <x-chama-x@users.noreply.github.com>
2024-09-14 18:34:25 -0300

    Inicio del proyecto

---
+++ src/main/java/com/mycompany/gym/web/project/java/primerServlet.java
@@ -0,0 +1,29 @@
+
+package com.mycompany.gym.web.project.java;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class primerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<html><head></head><body>");
+        out.println("<h1>Primer Servlet</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("post!!!");
+    }
+    
+    
+    
+    
+}
