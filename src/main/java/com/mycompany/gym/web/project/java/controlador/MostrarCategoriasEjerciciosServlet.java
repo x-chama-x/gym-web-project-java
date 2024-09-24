ca53504a727485e45fba97e1457265b5e1a05058
x_chama_x <x-chama-x@users.noreply.github.com>
2024-09-24 15:27:45 -0300

    feat: eliminé todo lo que tenga que ver con anotations (anotaciones) en el servlet mostrarCategoriasEjercicios.servlet

--- src/main/java/com/mycompany/gym/web/project/java/controlador/MostrarCategoriasEjerciciosServlet.java
+++ src/main/java/com/mycompany/gym/web/project/java/controlador/MostrarCategoriasEjerciciosServlet.java
@@ -2,7 +2,6 @@ package com.mycompany.gym.web.project.java.controlador;
 
 import com.mycompany.gym.web.project.java.modelo.ParteDelCuerpo;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
