3e324d697929abac8650a069c74c758f29331a16
x_chama_x <x-chama-x@users.noreply.github.com>
2024-10-04 19:26:19 -0300

    chore: mod y documentacion del servlet EditarEjercicioServlet.java

--- src/main/java/com/mycompany/gym/web/project/java/controlador/EditarEjercicioServlet.java
+++ src/main/java/com/mycompany/gym/web/project/java/controlador/EditarEjercicioServlet.java
@@ -1,10 +1,5 @@
 package com.mycompany.gym.web.project.java.controlador;
 
-import com.drew.imaging.ImageMetadataReader;
-import com.drew.imaging.ImageProcessingException;
-import com.drew.metadata.Metadata;
-import com.drew.metadata.MetadataException;
-import com.drew.metadata.exif.ExifIFD0Directory;
 import com.mycompany.gym.web.project.java.modelo.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.MultipartConfig;
@@ -13,10 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Part;
 
-import javax.imageio.ImageIO;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -30,6 +21,7 @@ public class EditarEjercicioServlet extends HttpServlet {
     private EquipoDAOHardCodeado equipoDAOHardCodeado;
     private ParteDelCuerpoDAOHardCodeado parteDelCuerpoDAOHardCodeado;
 
+    // inicializa el servlet y carga los ejercicios, equipos y partes del cuerpo
     @Override
     public void init() throws ServletException {
         ejercicioDAOHardCodeado = EjercicioDAOHardCodeado.getInstance();
@@ -37,6 +29,7 @@ public class EditarEjercicioServlet extends HttpServlet {
         parteDelCuerpoDAOHardCodeado = new ParteDelCuerpoDAOHardCodeado();
     }
 
+    // muestra la pagina editarEjercicio.jsp con los datos del ejercicio a editar
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
@@ -44,16 +37,22 @@ public class EditarEjercicioServlet extends HttpServlet {
             Ejercicio ejercicio = ejercicioDAOHardCodeado.getById(ejercicioId);
             ArrayList<ParteDelCuerpo> partesDelCuerpo = parteDelCuerpoDAOHardCodeado.getAll();
             ArrayList<Equipo> equipos = equipoDAOHardCodeado.getAll();
-            request.setAttribute("ejercicio", ejercicio);
-            request.setAttribute("partesDelCuerpo", partesDelCuerpo);
-            request.setAttribute("equipos", equipos);
-            request.setAttribute("equipoNombre", equipoDAOHardCodeado.getById(ejercicio.getEquipoID()).getNombre());
-            request.getRequestDispatcher("WEB-INF/jsp/editarEjercicio.jsp").forward(request, response);
+            String equipoNombre = equipoDAOHardCodeado.getById(ejercicio.getEquipoID()).getNombre();
+            redirigirAeditarEjercicio(request, response, ejercicio, partesDelCuerpo, equipos, equipoNombre);
         } catch (Exception e) {
             throw new ServletException("Error al cargar los datos para editar ejercicio", e);
         }
     }
 
+    private void redirigirAeditarEjercicio(HttpServletRequest request, HttpServletResponse response, Ejercicio ejercicio, ArrayList<ParteDelCuerpo> partesDelCuerpo, ArrayList<Equipo> equipos, String equipoNombre) throws ServletException, IOException {
+        request.setAttribute("ejercicio", ejercicio);
+        request.setAttribute("partesDelCuerpo", partesDelCuerpo);
+        request.setAttribute("equipos", equipos);
+        request.setAttribute("equipoNombre", equipoNombre);
+        request.getRequestDispatcher("WEB-INF/jsp/editarEjercicio.jsp").forward(request, response);
+    }
+
+    // edita el ejercicio con los datos recibidos y redirige a la pagina mostrarEjercicios.jsp
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int ejercicioId = Integer.parseInt(request.getParameter("ejercicioId"));
@@ -67,50 +66,39 @@ public class EditarEjercicioServlet extends HttpServlet {
         String equipoNombre = request.getParameter("equipo");
 
         try {
-            Ejercicio ejercicio = ejercicioDAOHardCodeado.getById(ejercicioId);
-            int parteDelCuerpoID = parteDelCuerpoDAOHardCodeado.getByName(musculoPrincipal).getParteDelCuerpoID();
-            int equipoID = equipoDAOHardCodeado.getByName(equipoNombre).getEquipoID();
+            Ejercicio ejercicio = ejercicioDAOHardCodeado.getById(ejercicioId); // obtengo el ejercicio a editar
+            int parteDelCuerpoID = parteDelCuerpoDAOHardCodeado.getByName(musculoPrincipal).getParteDelCuerpoID(); // obtengo el id de la parte del cuerpo
+            int equipoID = equipoDAOHardCodeado.getByName(equipoNombre).getEquipoID(); // obtengo el id del equipo
 
-            Part filePart = request.getPart("imagen");
-            if (filePart != null && filePart.getSize() > 0) {
-                String fileName = getFileName(filePart);
-                String filePath = "C:\\Users\\Francisco\\Desktop\\gym-web-project-java\\src\\main\\webapp\\assets\\img\\" + fileName;
-
-                // Save the file to the server
-                File file = new File(filePath);
-                file.getParentFile().mkdirs();
-                try (InputStream input = filePart.getInputStream()) {
-                    Files.copy(input, file.toPath());
-                }
-
-                // Correct the image orientation
-                try {
-                    correctImageOrientation(file);
-                } catch (ImageProcessingException | MetadataException e) {
-                    throw new RuntimeException(e);
-                }
-
-                ejercicio.setImagen(fileName);
-            }
-
-            ejercicio.setNombre(nombre);
-            ejercicio.setMusculosQueTrabaja(musculosQueTrabaja);
-            ejercicio.setPreparacion(preparacion);
-            ejercicio.setConsejosClave(consejosClave);
-            ejercicio.setDescripcion(descripcion);
-            ejercicio.setEjecucion(ejecucion);
-            ejercicio.setMusculoPrincipal(musculoPrincipal);
-            ejercicio.setParteDelCuerpoID(parteDelCuerpoID);
-            ejercicio.setEquipoID(equipoID);
-
-            ejercicioDAOHardCodeado.update(ejercicio);
+            Part filePart = request.getPart("imagen"); // obtengo la imagen
+            procesarImagen(filePart, ejercicio);
+            actualizarEjercicio(ejercicio, nombre, musculosQueTrabaja, preparacion, consejosClave, descripcion, ejecucion, musculoPrincipal, parteDelCuerpoID, equipoID);
+            ejercicioDAOHardCodeado.update(ejercicio); // actualizo el ejercicio en la base de datos
             response.sendRedirect("mostrarEjercicios?categoriaId=" + parteDelCuerpoID);
         } catch (Exception e) {
             throw new ServletException("Error al editar el ejercicio", e);
         }
     }
 
-    private String getFileName(Part part) {
+    // procesa la imagen recibida y la guarda en la carpeta assets/img
+    private void procesarImagen(Part filePart, Ejercicio ejercicio) throws IOException {
+        if (filePart != null && filePart.getSize() > 0) {
+            String fileName = obtenerNombreDelArchivo(filePart);
+            String uniqueFileName = System.currentTimeMillis() + "_" + fileName; // Genera un nombre de archivo único
+            String filePath = "C:\\Users\\Francisco\\Desktop\\gym-web-project-java\\src\\main\\webapp\\assets\\img\\" + uniqueFileName;
+
+            // guardar la imagen en la carpeta assets/img
+            File file = new File(filePath);
+            file.getParentFile().mkdirs();
+            try (InputStream input = filePart.getInputStream()) {
+                Files.copy(input, file.toPath());
+            }
+            ejercicio.setImagen(uniqueFileName);
+        }
+    }
+
+    // obtiene el nombre del archivo de la parte recibida
+    private String obtenerNombreDelArchivo(Part part) {
         String contentDisposition = part.getHeader("content-disposition");
         for (String cd : contentDisposition.split(";")) {
             if (cd.trim().startsWith("filename")) {
@@ -120,40 +108,16 @@ public class EditarEjercicioServlet extends HttpServlet {
         return null;
     }
 
-    private void correctImageOrientation(File file) throws IOException, ImageProcessingException, MetadataException {
-        Metadata metadata = ImageMetadataReader.readMetadata(file);
-        ExifIFD0Directory directory = metadata.getFirstDirectoryOfType(ExifIFD0Directory.class);
-        if (directory != null && directory.containsTag(ExifIFD0Directory.TAG_ORIENTATION)) {
-            int orientation = directory.getInt(ExifIFD0Directory.TAG_ORIENTATION);
-            BufferedImage image = ImageIO.read(file);
-            BufferedImage rotatedImage = rotateImage(image, orientation);
-            ImageIO.write(rotatedImage, "jpg", file);
-        }
-    }
-
-    private BufferedImage rotateImage(BufferedImage image, int orientation) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-        AffineTransform transform = new AffineTransform();
-        switch (orientation) {
-            case 1: // [ExifIFD0Directory.ORIENTATION_NORMAL]
-                return image;
-            case 6: // [ExifIFD0Directory.ORIENTATION_ROTATE_90]
-                transform.translate(height, 0);
-                transform.rotate(Math.toRadians(90));
-                break;
-            case 3: // [ExifIFD0Directory.ORIENTATION_ROTATE_180]
-                transform.translate(width, height);
-                transform.rotate(Math.toRadians(180));
-                break;
-            case 8: // [ExifIFD0Directory.ORIENTATION_ROTATE_270]
-                transform.translate(0, width);
-                transform.rotate(Math.toRadians(270));
-                break;
-            default:
-                return image;
-        }
-        AffineTransformOp op = new AffineTransformOp(transform, AffineTransformOp.TYPE_BILINEAR);
-        return op.filter(image, null);
+    // actualiza los datos del ejercicio
+    private void actualizarEjercicio(Ejercicio ejercicio, String nombre, String musculosQueTrabaja, String preparacion, String consejosClave, String descripcion, String ejecucion, String musculoPrincipal, int parteDelCuerpoID, int equipoID) {
+        ejercicio.setNombre(nombre);
+        ejercicio.setMusculosQueTrabaja(musculosQueTrabaja);
+        ejercicio.setPreparacion(preparacion);
+        ejercicio.setConsejosClave(consejosClave);
+        ejercicio.setDescripcion(descripcion);
+        ejercicio.setEjecucion(ejecucion);
+        ejercicio.setMusculoPrincipal(musculoPrincipal);
+        ejercicio.setParteDelCuerpoID(parteDelCuerpoID);
+        ejercicio.setEquipoID(equipoID);
     }
 }
